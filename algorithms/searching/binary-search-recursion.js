703de53892a15fb8c1ec7e639fde2c3de1145ac6
Virendra Kumar Patel <virendra.patel@news.co.uk>
2021-09-27 15:57:01 +0530

    Update binary-search-recursion.js

--- algorithms/searching/binary-search-recursion.js
+++ algorithms/searching/binary-search-recursion.js
@@ -25,4 +25,29 @@ function binarySearch(sortedArray, element, offset) {
   }
 }
 
-console.log(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9], 9, 0));
+// console.log(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9], 9, 0));
+
+function binarySearch2(sortedArray, element) {
+  function _sort(sortedArray, element, startIndex, endIndex) {
+    if (startIndex >= endIndex) {
+      return -1;
+    }
+
+    const middleIndex = startIndex + Math.floor((endIndex - startIndex) / 2);
+
+    const elementAtMiddle = sortedArray[middleIndex];
+    if (elementAtMiddle == element) {
+      return middleIndex;
+    }
+
+    if (elementAtMiddle < element) {
+      return _sort(sortedArray, element, middleIndex + 1, endIndex);
+    }
+
+    return _sort(sortedArray, element, startIndex, middleIndex - 1);
+  }
+
+  return _sort(sortedArray, element, 0, sortedArray.length - 1);
+}
+
+console.log("Found At Index :", binarySearch2([1, 2, 3, 4, 5, 6, 7, 8, 9], 7));
